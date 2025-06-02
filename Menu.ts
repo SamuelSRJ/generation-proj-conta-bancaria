cea412ad16f804ac6cdeae0fe5916d0ec925ae94
Samuel de Souza <samuelsouzarosajr@gmail.com>
2025-06-02 14:19:52 -0300

    Commit - Primeira parte do projeto concluída

---
+++ Menu.ts
@@ -0,0 +1,81 @@
+import readline from "readline-sync";
+
+export function main() {
+    let opcao: number;
+
+    while (true) {
+        console.log(
+            "\n *****************************************************\n",
+            "*************** BANCO DO BRAZIL COM Z ***************\n",
+            "*****************************************************\n",
+            "*            1 - Criar conta                        *\n",
+            "*            2 - Listar todas as Contas             *\n",
+            "*            3 - Buscar Conta por Número            *\n",
+            "*            4 - Atualizar Dados da Conta           *\n",
+            "*            5 - Apagar Conta                       *\n",
+            "*            6 - Sacar                              *\n",
+            "*            7 - Depositar                          *\n",
+            "*            8 - Transferir valores entre Contas    *\n",
+            "*            9 - Sair                               *\n",
+            "*****************************************************\n",
+        )
+        opcao = readline.questionInt(" Entre com a opção desejada: ");
+
+        if (opcao == 9) {
+            console.log("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+            // console.clear();
+            sobre();
+            process.exit(0);
+        }
+
+        switch (opcao) {
+            case 1:
+                console.log("\n Criar Conta");
+                break;
+
+            case 2:
+                console.log("\n Listar todas as Contas\n");
+                break;
+
+            case 3:
+                console.log("\n Consultar dados da Conta - por número\n");
+                break;
+            
+            case 4:
+                console.log("\n Atualizar dados da Conta\n");
+                break;
+
+            case 5:
+                console.log("\n Apagar uma conta\n");
+                break;
+
+            case 6:
+                console.log("\n Saque\n");
+                break;
+
+            case 7:
+                console.log("\n Depósito\n");
+                break;
+
+            case 8:
+                console.log("\n Transferência entre Contas\n");
+                break;
+            
+            default:
+                console.log("\n Opção Inválida\n");
+                break;
+        }
+    }
+}
+
+export function sobre(): void {
+    console.log(
+        " *****************************************************\n",
+        "*     Projeto Desenvolvido por: Samuel de Souza     *\n",
+        "*   Generation Brasil - generation@generation.org   *\n",
+        "*           github.com/conteudoGeneration           *\n",
+        "*****************************************************\n",
+    )
+}
+
+main()
